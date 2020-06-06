f4193a97d446db7bea073a4dbd5f3dbd5266e075
Angela Guardia <angelaguardia20@gmail.com>
2020-06-06 09:04:48 -0700

    Add nested collections work

--- data-types/collections/advanced_nested_collections/nesting.rb
+++ data-types/collections/advanced_nested_collections/nesting.rb
@@ -1,4 +1,4 @@
-def stores 
+def stores
   {
     olive_garden: {
       employees: ['Jeff', 'Zach', 'Samantha'],
@@ -42,5 +42,11 @@ def stores
                 price: 2}
               ]
             }
-  } 
+  }
+
+
 end
+
+
+# require 'pry'; binding.pry
+stores
